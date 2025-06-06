7d438776b91407592d0cc3378660d83dcf3a4f31
Aasif-at-git <imaasif.ai@gmail.com>
2025-06-06 16:25:47 +0400

    Update User.java

--- src/csc202/User.java
+++ src/csc202/User.java
@@ -13,15 +13,15 @@ public abstract class User {
     User() {
     }
 
-    public User(int userID, String userName, String userEmail, 
-            String userAddress, String userPassword, 
-            Coordinate locationCoordinates) {
-        this.userID = userID;
-        this.userName = userName;
-        this.userEmail = userEmail;
-        this.userAddress = userAddress;
-        this.userPassword = userPassword;
-        this.locationCoordinates = locationCoordinates;
+    public User(int userID, String userName, String userEmail,
+                String userAddress, String userPassword,
+                Coordinate locationCoordinates) {
+        setUserID(userID);
+        setUserName(userName);
+        setUserEmail(userEmail);
+        setUserAddress(userAddress);
+        setUserPassword(userPassword);
+        setLocationCoordinates(locationCoordinates);
     }
 
     public int getUserID() {
@@ -44,24 +44,59 @@ public abstract class User {
         return locationCoordinates;
     }
 
-    public void setLocationCoordinates(Coordinate locationCoordinates) {
-        this.locationCoordinates = locationCoordinates;
-    }
-
-    public String getStoredPassword() {
+    public String getUserPassword() {
         return userPassword;
-    }
-
-    public boolean userAuthentication(String enteredPassword) {
-        return this.userPassword.equals(enteredPassword);
     }
 
     public boolean getSeasonalUpdates() {
         return seasonalUpdates;
     }
 
+    public void setUserID(int userID) {
+        if (userID <= 0) {
+            throw new IllegalArgumentException("User ID can't be negative or zero.");
+        }
+        this.userID = userID;
+    }
+
+    public void setUserName(String userName) {
+        if(userName == null || userName.trim().isEmpty()){
+            throw new IllegalArgumentException("User Name can't be null or empty.");
+        }
+        this.userName = userName;
+    }
+
+    public void setUserEmail(String userEmail) {
+        if(userName.trim().contains("@")){
+            throw new IllegalArgumentException("Email ID should contain @.");
+        }
+        this.userEmail = userEmail;
+    }
+
+    public void setUserAddress(String userAddress) {
+        if(userAddress == null || userAddress.trim().isEmpty()){
+            throw new IllegalArgumentException("User Address can't be null or empty.");
+        }
+        this.userAddress = userAddress;
+    }
+
+    public void setUserPassword(String userPassword) {
+        if(userPassword == null || userPassword.trim().isEmpty()){
+            throw new IllegalArgumentException("User Password can't be null or empty.");
+        }
+        this.userPassword = userPassword;
+    }
+
+    public void setLocationCoordinates(Coordinate locationCoordinates) {
+        this.locationCoordinates = locationCoordinates;
+    }
+
     public void setSeasonalUpdates(boolean seasonalUpdates) {
         this.seasonalUpdates = seasonalUpdates;
+    }
+
+    public boolean userAuthentication(String enteredPassword) {
+        return this.userPassword.equals(enteredPassword);
     }
 
     public abstract void displayUser();
